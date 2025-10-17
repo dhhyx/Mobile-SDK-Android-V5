cad4d53af274ab5c7a2170679299890163317084
Huangshuai <2668382477@qq.com>
2025-10-17 17:25:19 +0800

    更新 5.16

--- SampleCode-V5/android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/communication/ObservableInMemoryKeyedStore.java
+++ SampleCode-V5/android-sdk-v5-uxsdk/src/main/java/dji/v5/ux/core/communication/ObservableInMemoryKeyedStore.java
@@ -57,7 +57,7 @@ public class ObservableInMemoryKeyedStore implements ObservableKeyedStore {
     }
 
     public static ObservableInMemoryKeyedStore getInstance() {
-        return ObservableInMemoryKeyedStore.SingletonHolder.instance;
+        return SingletonHolder.instance;
     }
 
     /**
